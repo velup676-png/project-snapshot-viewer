d300519d5bf8c8f6636c1f26ef2f21961a07bfb4
velup676-png <velup676@gmail.com>
2026-08-17 22:15:31 +0530

    Add atmospheric ash-black transition panel after hero

---
+++ src/components/site/AtmospherePanel.tsx
@@ -0,0 +1,8 @@
+export function AtmospherePanel() {
+  return (
+    <section className="atmosphere-panel" aria-hidden="true">
+      <div className="atmosphere-panel__wash" />
+      <div className="atmosphere-panel__grain" />
+    </section>
+  );
+}
